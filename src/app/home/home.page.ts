f577ff47ab635ca45be9f1c03bcef16a0819dfec
Lucas Renó <lucasreno9@gmail.com>
2022-05-18 20:41:24 -0300

    Adicionado produtos por categoria

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Categoria } from '../categoria';
+import { Produto } from '../produto';
 
 @Component({
   selector: 'app-home',
@@ -19,6 +20,13 @@ export class HomePage {
     this.http.get<Categoria[]>(this.BASE_URL+'/categorias').subscribe(
       resposta => {
         this.categorias = resposta;
+        this.categorias.forEach(cat => {
+          this.http.get<Produto[]>(this.BASE_URL+'/produtos/'+cat.idCategoria).subscribe(
+            resp => {
+              cat.produtos = resp;
+            }
+          );
+        });
       }
     );
   }
