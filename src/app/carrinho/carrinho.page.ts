f8e5b7f798ec889d7201fc3df6723f94b927b336
Lucas Renó <lucasreno9@gmail.com>
2022-06-23 21:28:48 -0300

    Carregado itens no carrinho

--- src/app/carrinho/carrinho.page.ts
+++ src/app/carrinho/carrinho.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
+import { Carrinho } from '../carrinho';
 
 @Component({
   selector: 'app-carrinho',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carrinho.page.scss'],
 })
 export class CarrinhoPage implements OnInit {
+  carrinho: Carrinho[] = [];
+  totalCarrinho: number = 0;
 
-  constructor() { }
+  constructor(private storage: Storage, private router: Router) { }
 
   ngOnInit() {
   }
 
+  async iniciarBanco() {
+    this.storage.create();
+    this.carrinho = await this.storage.get('carrinho') ?? [];
+    this.totalCarrinho = 0;
+    this.carrinho.forEach(c => {
+      this.totalCarrinho += c.quantidade * c.produto.valor;
+    });
+  }
+
+  ionViewWillEnter() {
+    this.iniciarBanco();
+  }
+
+  remover(indice) {
+    this.carrinho.splice(indice, 1);
+    this.storage.set('carrinho', this.carrinho);
+    
+    if (this.carrinho.length == 0)
+      this.router.navigateByUrl('/');
+
+    this.totalCarrinho = 0;
+    this.carrinho.forEach(c => {
+      this.totalCarrinho += c.quantidade * c.produto.valor;
+    });
+  }
 }
